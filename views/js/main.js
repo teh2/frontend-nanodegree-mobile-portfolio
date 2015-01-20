408a77d3c92a4c2639ee03cad213205276411ce6
tom <thillii@yahoo.com>
2015-01-19 19:55:28 -0600

    Remove redundant code from for loop inupdatePositions to speed up rendering.

--- views/js/main.js
+++ views/js/main.js
@@ -505,9 +505,12 @@ function updatePositions() {
   window.performance.mark("mark_start_frame");
 
   var items = document.querySelectorAll('.mover');
+  var phase = new Array(5);
+  for (var j = 0; j < 5; j++) {
+    phase[j] = 100 * Math.sin((document.body.scrollTop / 1250) + j);
+  }
   for (var i = 0; i < items.length; i++) {
-    var phase = Math.sin((document.body.scrollTop / 1250) + (i % 5));
-    items[i].style.left = items[i].basicLeft + 100 * phase + 'px';
+    items[i].style.left = items[i].basicLeft + phase[i % 5] + 'px';
   }
 
   // User Timing API to the rescue again. Seriously, it's worth learning.
